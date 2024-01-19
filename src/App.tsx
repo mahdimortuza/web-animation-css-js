4c36cfeb1ed65cf4cdaa7e9595764bac73f32da9
Mahdi Mortuza <mahdi.webx@gmail.com>
2024-01-19 22:24:15 +0600

    clip path transition

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,20 @@ import "./App.css";
 function App() {
   return (
     <div className="main">
-      <div className="loading">
-        <div className="glass"></div>
-        <div className="box"></div>
+      <div className="card">
+        <h1>heading</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum modi
+          esse voluptatem voluptas quae ipsum officiis laborum.
+        </p>
       </div>
 
+      {/*
+      // loading animation
+       <div className="loading">
+        <div className="glass"></div>
+        <div className="box"></div>
+         */}
       {/*<div className="box-1"></div>
       <div className="box-2"></div>
        <button className="btn">hover me</button> */}
